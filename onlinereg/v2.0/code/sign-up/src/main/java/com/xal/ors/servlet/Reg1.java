4e3719b9995bd5e8bb396b68e86af49f8110f3b5
3203317 <3203317@qq.com>
2014-06-20 12:16:15 +0800

    haha

---
+++ onlinereg/v2.0/code/sign-up/src/main/java/com/xal/ors/servlet/Reg1.java
@@ -0,0 +1,38 @@
+package com.xal.ors.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Reg1 extends HttpServlet {
+	private static final long serialVersionUID = -7242279630539832052L;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Hello World!");
+	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setContentType("application/json");
+
+		PrintWriter out = response.getWriter();
+		out.write("haha");
+		out.flush();
+		out.close();
+	}
+
+}
