9a40b7dfa0333758ab10e8743809be2893cf4a44
3203317 <3203317@qq.com>
2014-06-26 12:03:23 +0800

    haha

---
+++ onlinereg/v2.0/code/sign-up/src/main/java/com/xal/ors/biz/MgrService.java
@@ -0,0 +1,8 @@
+package com.xal.ors.biz;
+
+import com.xal.ors.ResultMapper;
+import com.xal.ors.model.Manager;
+
+public interface MgrService {
+	ResultMapper changePw(Manager mgr);
+}
---
+++ onlinereg/v2.0/code/sign-up/src/main/java/com/xal/ors/model/Manager.java
@@ -0,0 +1,23 @@
+package com.xal.ors.model;
+
+public class Manager {
+	private String userName;
+	private String userPass;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPass() {
+		return userPass;
+	}
+
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+
+}
